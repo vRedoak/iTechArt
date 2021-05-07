9c4eee53f32a386e8ebf1f188d16151357f3787e
krasnodubskaya.v@gmail.com <krasnodubskaya.v@gmail.com>
2021-05-07 15:31:12 +0300

    Remove and sort usings

--- Task1_Logger/Logger/Logger/SimpleLogger/ILoggerProvider.cs
+++ Task1_Logger/Logger/Logger/SimpleLogger/ILoggerProvider.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.Extensions.Logging;
-
-namespace MyLogger.SimpleLogger
+﻿namespace MyLogger.SimpleLogger
 {
     public interface ILoggerProvider
     {
