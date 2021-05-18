4a33d409724a49cb95ced6a0537e5c61d3791595
krasnodubskaya.v@gmail.com <krasnodubskaya.v@gmail.com>
2021-05-18 15:49:04 +0300

    Changed work with UnitOfWork

--- FromCsvToDatabase/FromCsvToDatabase/Program.cs
+++ FromCsvToDatabase/FromCsvToDatabase/Program.cs
@@ -12,10 +12,11 @@ namespace FromCsvToDatabase
         {
             string connectionString = @"Data Source = WSA-112-21\SQL_EXPRESS; Initial Catalog = Zoo; Integrated Security = True; User Id = ICX\V.Krasnadubskaya";
             string filePath = @"../../../Animals.csv";
+            var unitOfWork = new UnitOfWork(connectionString);
 
-           WriteAnimalsFromCsvToDataBase(filePath, connectionString);
+            WriteAnimalsFromCsvToDataBase(filePath, unitOfWork);
 
-            GetWorkersFromDataBaseToConsole(connectionString);
+            GetWorkersFromDataBaseToConsole(unitOfWork);
             Console.WriteLine("loading...");
 
             Console.Read();
@@ -23,22 +24,17 @@ namespace FromCsvToDatabase
 
 
         // a couple of methods to test work
-        static void WriteAnimalsFromCsvToDataBase(string csvFilePath, string connectionString)
+        static void WriteAnimalsFromCsvToDataBase(string csvFilePath, UnitOfWork unitOfWork)
         {
-            using var unitOfWork = new UnitOfWork(connectionString);
             foreach (var record in new CsvEnumerable<Animals>(csvFilePath, ';'))
             {
                 unitOfWork.Animals.Create(record);
             }
         }
 
-        static async void GetWorkersFromDataBaseToConsole(string connectionString)
+        static async void GetWorkersFromDataBaseToConsole(UnitOfWork unitOfWork)
         {
-            IEnumerable<Workers> allWorkers;
-            using (var unitOfWork = new UnitOfWork(connectionString))
-            {
-                allWorkers = await unitOfWork.Workers.GetListAsync();
-            }
+            var allWorkers = await unitOfWork.Workers.GetListAsync();
             foreach (var worker in allWorkers)
                 Console.WriteLine(worker.name);
         }
